321fd235db729c07d7c76ef54df8767c390a8343
mastersu83 <master_su@mail.ru>
2021-06-14 23:18:19 +0300

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
+let openMenu = document.querySelector('.header__btn');
+let menu = document.querySelector('.rightside-menu');
+let closeMenu = document.querySelector('.rightside-menu__close');
+
+function showMenu() {
+	menu.classList.remove('rightside-menu--close');
+}
+
+function hideMenu() {
+	menu.classList.add('rightside-menu--close');
+}
+
+openMenu.addEventListener('click', showMenu);
+closeMenu.addEventListener('click', hideMenu);
+
+$('.blog__slider').slick({
+	slidesToShow: 1,
+	dots: true,
+	arrows: true,
+});
+$('.hero__slider').slick({
+	slidesToShow: 1,
+	dots: true,
+	arrows: false,
+});
+
 const commentContainer = document.querySelector('.comments__items');
 const commentForm = document.querySelector('#formAdd');
 const commentInputText = document.querySelector('.comment__input-text');
